767964f8608ce3a8888cdc0d5d2cc5f1219627fa
Jay <hzhjjie@163.com>
2015-11-18 23:48:23 +0800

    add Class_01_JSPatch

---
+++ JJ_Class_01_热更新/JJ_JSPatch/JJ_JSPatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJ_JSPatch
+//
+//  Created by Jay on 15/11/18.
+//  Copyright © 2015年 JJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JJ_Class_01_热更新/JJ_JSPatch/JJ_JSPatch/ViewController.m
@@ -0,0 +1,21 @@
+//
+//  ViewController.m
+//  JJ_JSPatch
+//
+//  Created by Jay on 15/11/18.
+//  Copyright © 2015年 JJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
