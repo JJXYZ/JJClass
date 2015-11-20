5777004a33cbe073bb4160735652f8b073003499
Jay <hzhjjie@163.com>
2015-11-20 14:36:29 +0800

    add MJExtension

---
+++ JJ_Class_03_对象模型/JJ_MJExtension/JJ_MJExtension/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  JJ_MJExtension
+//
+//  Created by Jay on 15/11/20.
+//  Copyright © 2015年 JJ. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+@end
---
+++ JJ_Class_03_对象模型/JJ_MJExtension/JJ_MJExtension/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  JJ_MJExtension
+//
+//  Created by Jay on 15/11/20.
+//  Copyright © 2015年 JJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+@end
