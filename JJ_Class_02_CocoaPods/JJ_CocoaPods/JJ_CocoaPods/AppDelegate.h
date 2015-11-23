4a8d12a9f163c03c7b9381f6b61da0517cdbb88c
Jay <hzhjjie@163.com>
2015-11-23 10:09:16 +0800

    add pods

---
+++ JJ_Class_02_CocoaPods/JJ_CocoaPods/JJ_CocoaPods/AppDelegate.h
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.h
+//  JJ_CocoaPods
+//
+//  Created by Jay on 15/11/23.
+//  Copyright © 2015年 JJ. All rights reserved.
+//
+
+/**
+ *  唐巧:用CocoaPods做iOS程序的依赖管理
+    blog.devtang.com/blog/2014/05/25/use-cocoapod-to-manage-ios-lib-dependency/
+ */
+
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JJ_Class_02_CocoaPods/JJ_CocoaPods/JJ_CocoaPods/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  JJ_CocoaPods
+//
+//  Created by Jay on 15/11/23.
+//  Copyright © 2015年 JJ. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+
+@end
