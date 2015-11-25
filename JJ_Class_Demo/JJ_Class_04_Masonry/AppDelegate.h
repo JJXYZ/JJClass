06133157828044a24b15c7eb2e886934bdc449b6
JJ <hzhjjie@163.com>
2015-11-25 15:51:56 +0800

    add Autolayout

---
+++ JJ_Class_Demo/JJ_Class_04_Masonry/AppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.h
+//  JJ_Class_04_Masonry
+//
+//  Created by Jay on 15/11/25.
+//  Copyright © 2015年 JJ. All rights reserved.
+//
+
+
+
+/**
+ *  github:Masonry
+ */
+
+
+
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JJ_Class_Demo/JJ_Class_04_Masonry/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JJ_Class_04_Masonry
+//
+//  Created by Jay on 15/11/25.
+//  Copyright © 2015年 JJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
