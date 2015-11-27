6dd443d8af946e5b2cb0fb9948fb14f6febe58fe
JJ <hzhjjie@163.com>
2015-11-27 14:33:29 +0800

    modify pod file

--- JJ_Class_Demo/JJ_Class_07_ReactiveCocoa/ViewController.m
+++ JJ_Class_Demo/JJ_Class_07_ReactiveCocoa/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @interface ViewController ()
 
--- JJ_Class_Demo/JJ_Class_07_ReactiveCocoa_Tutorial/ReactivePlayground-Starter/RWReactivePlayground/RWViewController.m
+++ JJ_Class_Demo/JJ_Class_07_ReactiveCocoa_Tutorial/ReactivePlayground-Starter/RWReactivePlayground/RWViewController.m
@@ -8,6 +8,7 @@
 
 #import "RWViewController.h"
 #import "RWDummySignInService.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @interface RWViewController ()
 
