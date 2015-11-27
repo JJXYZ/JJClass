ce1562b96b356834c799ebb0ea534cf7a1943998
JJ <hzhjjie@163.com>
2015-11-27 17:55:16 +0800

    modify RWReactivePlayground

--- JJ_Class_Demo/JJ_Class_07_ReactiveCocoa_Tutorial/ReactivePlayground-Starter/RWReactivePlayground/RWDummySignInService.h
+++ JJ_Class_Demo/JJ_Class_07_ReactiveCocoa_Tutorial/ReactivePlayground-Starter/RWReactivePlayground/RWDummySignInService.h
@@ -12,6 +12,9 @@ typedef void (^RWSignInResponse)(BOOL);
 
 @interface RWDummySignInService : NSObject
 
+/**
+ *  这个方法带有一个用户名、密码和一个完成block。block会在登录成功或失败时调用。我们可以在subscribeNext:块中直接调用这个方法，但为什么不呢？因为这是一个异步操作，小心了。
+ */
 - (void)signInWithUsername:(NSString *)username password:(NSString *)password complete:(RWSignInResponse)completeBlock;
 
 @end
