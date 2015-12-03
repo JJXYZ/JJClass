1d87f501c4dbcd8c71ec732b5f0ce8df03677a3a
JJ <hzhjjie@163.com>
2015-12-03 15:47:22 +0800

    add 编译错误

---
+++ JJ_Class_Demo/JJ_Class_01_笔记/编译错误.h
@@ -0,0 +1,42 @@
+//
+//  编译错误.h
+//  JJ_Class_Demo
+//
+//  Created by Jay on 15/12/3.
+//  Copyright © 2015年 JJ. All rights reserved.
+//
+
+/**
+ *  
+ 
+ 
+ ====================================================================
+ 
+ 百度经验:Xcode7报错
+ http://jingyan.baidu.com/article/a3aad71ac4de98b1fb0096ac.html
+ 
+ CocoaChina:升级xcode7后提示警告如何解决   
+ http://www.cocoachina.com/bbs/read.php?tid-328574.html
+ 
+ Other linker Flags:-w
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
