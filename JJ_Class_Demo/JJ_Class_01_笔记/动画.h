7e6283a3020b3d8c80912fbc8cf8b053d0377f01
JJ <hzhjjie@163.com>
2015-12-09 09:29:18 +0800

    add 动画

---
+++ JJ_Class_Demo/JJ_Class_01_笔记/动画.h
@@ -0,0 +1,37 @@
+//
+//  动画.h
+//  JJ_Class_Demo
+//
+//  Created by Jay on 15/12/8.
+//  Copyright © 2015年 JJ. All rights reserved.
+//
+
+#ifndef ___h
+#define ___h
+
+
+
+/**
+ 
+    ============================CATransition========================================
+ 
+    Sparta-301: iOS:UIimageView切换图片渐入渐出的动画
+    http://blog.csdn.net/ksws0393238aa/article/details/21297823
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
+
+
+
+
+
+
+
+
+#endif /* ___h */
