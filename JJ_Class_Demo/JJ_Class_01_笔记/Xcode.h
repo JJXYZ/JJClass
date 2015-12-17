feb5f83f5461bfe1c21f6f4587b39c9974efb450
JJ <hzhjjie@163.com>
2015-12-17 09:37:02 +0800

    add Xcode

--- JJ_Class_Demo/JJ_Class_01_笔记/Xcode.h
+++ JJ_Class_Demo/JJ_Class_01_笔记/Xcode.h
@@ -12,6 +12,11 @@
 /**
  *  陈斌彬的技术博客:Xcode6.3+iOS 8.1免证书开发+真机调试+生成ipa(原创)
     http://cnbin.github.io/blog/2015/05/20/xcode6-dot-3-plus-ios-8-dot-1mian-zheng-shu-kai-fa-plus-zhen-ji-diao-shi-plus-sheng-cheng-ipa/
+ 
+ 
+    Kingiol Ding:程序支持不同iOS的版本和设备
+    http://blog.kingiol.com/blog/2013/09/05/supporting-multiple-ios-versions-and-devices/
+ 
  */
 
 
