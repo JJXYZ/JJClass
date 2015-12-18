78915e8431f0edbe2f91fd4bcb9bd2f74dd77c6a
JJ <hzhjjie@163.com>
2015-12-18 18:06:18 +0800

    add

---
+++ JJ_Class_Demo/JJ_Class_01_笔记/HTTP.h
@@ -0,0 +1,12 @@
+//
+//  HTTP.h
+//  JJ_Class_Demo
+//
+//  Created by Jay on 15/12/18.
+//  Copyright © 2015年 JJ. All rights reserved.
+//
+
+/**
+ *  iOS安全系列之一：HTTPS
+    http://oncenote.com/2014/10/21/Security-1-HTTPS/
+ */
