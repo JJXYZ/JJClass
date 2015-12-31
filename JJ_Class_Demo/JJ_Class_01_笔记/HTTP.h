b9c8ebbef8c93b3ff16d191aa1543ad8474acfab
JJ <hzhjjie@163.com>
2015-12-31 18:15:51 +0800

    add

--- JJ_Class_Demo/JJ_Class_01_笔记/HTTP.h
+++ JJ_Class_Demo/JJ_Class_01_笔记/HTTP.h
@@ -9,4 +9,16 @@
 /**
  *  iOS安全系列之一：HTTPS
     http://oncenote.com/2014/10/21/Security-1-HTTPS/
+ 
+ 
+ 伯乐在线:CocoaSPDY：Twitter推出用于iOS和Mac OS X的SPDY框架
+ http://blog.jobbole.com/57867/
+ 
+ 
+ 正确使用AFNetworking的SSL保证网络安全
+ http://www.jianshu.com/p/4102b817ff2f
+ 
+ 
+ Github:iOS9AdaptationTips
+ 
  */
