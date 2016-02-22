5c9c1e040ce811ee58dc879376f6bb5a85a84535
JJ <hzhjjie@163.com>
2016-02-22 14:22:47 +0800

    add

---
+++ JJ_Class_Demo/JJ_Class_13_socket/JJ_Class_13_socket.h
@@ -0,0 +1,18 @@
+//
+//  JJ_Class_13_socket.h
+//  JJ_Class_Demo
+//
+//  Created by Jay on 16/2/22.
+//  Copyright © 2016年 JJ. All rights reserved.
+//
+
+#ifndef JJ_Class_13_socket_h
+#define JJ_Class_13_socket_h
+
+
+/**
+    SocketIO --- iOS使用篇
+ *  http://blog.csdn.net/thelittleboy/article/details/46538335
+ */
+
+#endif /* JJ_Class_13_socket_h */
